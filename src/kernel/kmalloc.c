f42fe25b7a05f328d54b7bdb3f342a4b9079eb45
philip-nielsen <philipnielsen64@gmail.com>
2026-05-15 17:50:04 +0200

    improved kmalloc

--- src/kernel/kmalloc.c
+++ src/kernel/kmalloc.c
@@ -31,12 +31,12 @@ uint32_t malloc(size_t bytes_to_allocate) {
 
         if (size >= real_size) {
 
-            uint32_t remainging_space = size - real_size;
+            uint32_t remaining_space = size - real_size;
 
-            if (remainging_space >= sizeof(block_header_t) + 4) {
+            if (remaining_space >= sizeof(block_header_t) + 4) {
                 block_header_t *new_block =
                     (block_header_t *)((uint32_t)current + real_size);
-                new_block->size_and_flags = remainging_space;
+                new_block->size_and_flags = remaining_space;
                 new_block->size_and_flags |= 0x1;
 
                 current->size_and_flags = real_size;
@@ -66,26 +66,36 @@ uint32_t malloc(size_t bytes_to_allocate) {
     }
 
     uint32_t new_block_addr = heap_watermark;
-
     uint32_t pages_needed = (real_size + 4095) / 4096;
+
     for (uint32_t i = 0; i < pages_needed; i++) {
         alloc_page(heap_watermark, PTE_PRESENT | PTE_RW);
         heap_watermark += 4096;
     }
 
+    uint32_t remaining_space = pages_needed - real_size;
     block_header_t *new_block = (block_header_t *)new_block_addr;
 
-    new_block->size_and_flags = (pages_needed * 4096);
-    new_block->size_and_flags |= 0x1;
-    new_block->next_free_slot = 0;
+    if (remaining_space >= sizeof(block_header_t) + 4) {
+        block_header_t *last_block =
+            (block_header_t *)(new_block_addr + real_size);
+        last_block->size_and_flags = remaining_space;
+        last_block->size_and_flags |= 0x1;
+        last_block->next_free_slot = 0;
 
-    if (free_list_head == 0) {
-        free_list_head = new_block;
+        new_block->size_and_flags = real_size;
+        new_block->size_and_flags &= ~0x1;
+
+        if (free_list_head == 0) {
+            free_list_head = last_block;
+        } else {
+            previous->next_free_slot = last_block;
+        }
     } else {
-        previous->next_free_slot = new_block;
+        new_block->size_and_flags = pages_needed & ~0x1;
     }
 
-    return malloc(bytes_to_allocate);
+    return new_block_addr + sizeof(block_header_t);
 }
 
 void kfree(uint32_t address) {
